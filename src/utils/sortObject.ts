f16337ac52a98dd28f005495dba7f7229bb20c95
Namee2810 <dvn281002@gmail.com>
2022-09-06 13:22:06 +0700

    improve: sort object

--- src/utils/sortObject.ts
+++ src/utils/sortObject.ts
@@ -1,14 +1,8 @@
 export function sortObject(obj: Record<string, string | number | boolean>) {
-  const sorted = {}
-  const str = []
-  for (const key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      str.push(encodeURIComponent(key))
-    }
-  }
-  str.sort()
-  for (let key = 0; key < str.length; key++) {
-    sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+")
-  }
-  return sorted
+  return Object.keys(obj)
+    .sort()
+    .reduce(function (sorted, key) {
+      sorted[key] = encodeURIComponent(obj[key]).replace(/%20/g, "+")
+      return sorted
+    }, {})
 }
